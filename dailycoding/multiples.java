13934869e76509c42f9601950dd89e3f5ec0bcd6
lucas <124001904+oinwajopwa@users.noreply.github.com>
2024-09-12 20:06:14 +0000

    commit

--- dailycoding/multiples.java
+++ dailycoding/multiples.java
@@ -1,3 +1,21 @@
-public class multiples {
-    
+import java.util.Scanner;
+
+class multiples {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Please pick 5 values you want to multiply.");
+        int value1 = scan.nextInt();
+        int value2 = scan.nextInt();
+        int value3 = scan.nextInt();
+        int value4 = scan.nextInt();
+        int value5 = scan.nextInt();
+        System.out.println("Now, pick the number you want to multiply them by.");
+        int multiplier = scan.nextInt();
+        int value11 = value1 * multiplier;
+        int value22 = value2 * multiplier;
+        int value33 = value3 * multiplier;
+        int value44 = value4 * multiplier;
+        int value55 = value5 * multiplier;
+        System.out.println(value11 + " " + value22 + " " + value33 + " " + value44 + " " + value55);
+    }
 }
