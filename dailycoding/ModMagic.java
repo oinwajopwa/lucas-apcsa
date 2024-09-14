b1d416d16a569a3a9973a17f44b7b87a520c2c58
lucas <124001904+oinwajopwa@users.noreply.github.com>
2024-09-14 06:25:26 +0000

    commit

--- dailycoding/ModMagic.java
+++ dailycoding/ModMagic.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class ModMagic {
-    Scanner scan = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args){
         System.out.println("Please input a value greater than 10.");
+        int value = scan.nextInt();
+        System.out.printf("%d %d %d %d %d %d %d %d\n", value % 2, value % 3, value % 4, value % 5, value % 6, value % 7, value % 8, value % 9);
     }
 }
