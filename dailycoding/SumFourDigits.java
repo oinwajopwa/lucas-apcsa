d698685f195a88630f63452afd87d038bdf90cf1
lucas <124001904+oinwajopwa@users.noreply.github.com>
2024-09-18 04:49:01 +0000

    commit

--- dailycoding/SumFourDigits.java
+++ dailycoding/SumFourDigits.java
@@ -5,11 +5,7 @@ class SumFourDigits {
     public static void main(String[] args) {
         System.out.println("Please input a 4 digit number.");
         int number = scan.nextInt();
-        int val1 = (number) % 10;
-        int val2 = (number/10) % 10;
-        int val3 = (number/100) % 10;
-        int val4 = (number/1000) % 10;
-        int sum = val1 + val2 + val3 + val4;
+        int sum = ((number) % 10) + ((number/10) % 10)+ ((number/100) % 10) + ((number/1000) % 10);
         System.out.println("The sum of " + number + " is " + sum + ".");
     }
 }
