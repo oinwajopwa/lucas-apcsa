9fe140dbcdf5ac49f5892bd67a364589c4945e8f
lucas <124001904+oinwajopwa@users.noreply.github.com>
2024-09-24 15:57:34 +0000

    commit

--- dailycoding/DaysUtil.java
+++ dailycoding/DaysUtil.java
@@ -5,8 +5,10 @@ public class DaysUtil {
     public static void main(String[] args){
         System.out.println("Input the amount of days to convert to minutes.");
         int days = scan.nextInt();
-        int minutes = (days * 24 * 60);
+        int minutes = 36059;
+        int minutes1 = minutes % 60;
+        int hours = (minutes / 60) % 60;
         System.out.println("There are " + minutes + " minutes in " + days + " days.");
-        System.out.println(minutes + " minutes is equal to " + days + " days, " + (minutes % 60) + " hour(s)," + " and " + (minutes % 60) + " minute(s).");
+        System.out.println(minutes + " minutes is equal to " + days + " days, " + hours + " hour(s)," + " and " + minutes1 + " minute(s).");
     }
 }
