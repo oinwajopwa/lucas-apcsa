c093fc2837f9f772c04e68f988a187b39fe4bae2
lucas <124001904+oinwajopwa@users.noreply.github.com>
2024-09-30 19:33:49 +0000

    commit

--- dailycoding/BeforeAndAfterv2.java
+++ dailycoding/BeforeAndAfterv2.java
@@ -7,13 +7,16 @@ public class BeforeAndAfterv2 {
         int value = scan.nextInt();
         System.out.println("Please select a letter.");
         char letter = scan.next().charAt(0);
-        char before = (char) (letter - 1);
+        char before1 = (char) (letter - 2);
+        char before2 = (char) (letter - 1);
+        char before3 = (char) (letter + 1);
+        char before4 = (char) (letter + 2);
 
         int valueb1 = value - 1;
         int valueb2 = value - 2;
         int valuea1 = value + 1;
         int valuea2 = value + 2;
         System.out.println(valueb2 + " " + valueb1 + " " + value + " " + valuea1 + " " + valuea2);
-        System.out.println(before);
+        System.out.println(before1 + " " + before2 + " " + letter + " " + before3 + " " + before4);
     }
 }
