fa83388bc1694f53a09a4ceeb7a07ffdfa027e98
lucas <124001904+oinwajopwa@users.noreply.github.com>
2024-10-07 20:13:43 +0000

    commit

--- dailycoding/PartsOfWord.java
+++ dailycoding/PartsOfWord.java
@@ -6,5 +6,6 @@ public class PartsOfWord {
         System.out.println("Enter a four letter word.");
         String word = scan.nextLine();
         System.out.println(word.substring(0,1) + " " + word.substring(0,2) + " " + word.substring(0,3) + " " + word.substring(0, 4) + " " + word.substring(1, 4) + " " + word.substring(2, 4) + " " + word.substring(3, 4) + " " + word.substring(1, 3));
+        scan.close();
     }
 }
