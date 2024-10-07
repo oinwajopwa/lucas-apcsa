abb1669a3923302a5685d1052c0ae1ef6870c52f
lucas <124001904+oinwajopwa@users.noreply.github.com>
2024-10-07 20:13:18 +0000

    commit

---
+++ dailycoding/PartsOfWord.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class PartsOfWord {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a four letter word.");
+        String word = scan.nextLine();
+        System.out.println(word.substring(0,1) + " " + word.substring(0,2) + " " + word.substring(0,3) + " " + word.substring(0, 4) + " " + word.substring(1, 4) + " " + word.substring(2, 4) + " " + word.substring(3, 4) + " " + word.substring(1, 3));
+    }
+}
