dcd68376d1a91b3e462ced52adbb148c3ebd7ac5
lucas <124001904+oinwajopwa@users.noreply.github.com>
2024-10-09 06:10:13 +0000

    commit

---
+++ dailycoding/Halves.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Halves {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please input a sentence.");
+        String sentence = scan.nextLine();
+        System.out.println("Now, grab a word from that sentence.");
+        String word = scan.nextLine();
+        //System.out.println(sentence.indexOf(word));
+        System.out.println("First Half: " + sentence.substring(0, sentence.indexOf(word)));
+        System.out.println("Second Half: " + sentence.substring(sentence.indexOf(word)));
+    }
+}
